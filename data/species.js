2d5a95892f5abd0523fb9102ac79372e564f90f4
SilverZephyr42 <127065327+SilverZephyr42@users.noreply.github.com>
2026-05-03 02:38:53 +1000

    Update species.js

--- data/species.js
+++ data/species.js
@@ -181,7 +181,7 @@ exports.BattlePokedex = {
         "forme": "Mega",
         "tier": "unobtainable"
     },
-    "venusaurmegag": {
+    "venusaurgmax": {
         "num": 3,
         "baseStats": {
             "hp": 90,
