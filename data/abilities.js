ecfe6414860024e00da594a37f6b38b6a142da52
SilverZephyr42 <127065327+SilverZephyr42@users.noreply.github.com>
2026-05-06 22:04:28 +1000

    Update abilities.js

--- data/abilities.js
+++ data/abilities.js
@@ -44,6 +44,17 @@ exports.BattleAbilities = {
         "desc": "Being hit with a Flying-type move boosts the user's Speed by 1 stage instead of dealing damage. While this Pokemon is active, the effects of weather conditions are disabled.",
         "shortDesc": "Flying moves boost Speed instead of damaging. While this Pokemon is active, the effects of weather conditions are disabled."
     },
+    "punkrock": {
+        "onBasePowerPriority": 7,
+        "flags": {
+            "breakable": 1
+        },
+        "name": "Punk Rock",
+        "rating": 3.5,
+        "num": 244,
+        "desc": "This Pokemon's sound-based moves have their power multiplied by 1.3. This Pokemon takes halved damage from sound-based moves.",
+        "shortDesc": "This Pokemon receives 1/2 damage from sound moves. Its own have 1.3x power."
+    },
     "analytic": {
         "onBasePowerPriority": 21,
         "flags": {},
@@ -1850,17 +1861,6 @@ exports.BattleAbilities = {
         "num": 227,
         "desc": "On switch-in, this Pokemon summons Psychic Terrain.",
         "shortDesc": "On switch-in, this Pokemon summons Psychic Terrain."
-    },
-    "punkrock": {
-        "onBasePowerPriority": 7,
-        "flags": {
-            "breakable": 1
-        },
-        "name": "Punk Rock",
-        "rating": 3.5,
-        "num": 244,
-        "desc": "This Pokemon's sound-based moves have their power multiplied by 1.3. This Pokemon takes halved damage from sound-based moves.",
-        "shortDesc": "This Pokemon receives 1/2 damage from sound moves. Its own have 1.3x power."
     },
     "purepower": {
         "onModifyAtkPriority": 5,
